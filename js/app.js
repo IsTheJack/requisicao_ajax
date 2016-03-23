882a7e262059c61938e69733836004f5dbc937f6
Roberto <robertooliveiraestudo@gmail.com>
2016-03-23 16:56:02 -0300

    Tirando aspas duplas e colocando simples

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 (function($, jQuery) {
-    "use strict";
+    'use strict';
     // Ao carregar e renderizar todos os elementos da página, fazer:
     $(window).load(function() {
         // Adicionando evento de clique no botão e inserindo a lógica
-        $(".btnSearch").click(function() {
+        $('.btnSearch').click(function() {
             $.ajax({
-                method: "GET",
+                method: 'GET',
                 // url recebe a chamada de uma função anônima para deixar a string flexível às mudanças de valores do textbox
-                url: "http://republicavirtual.com.br/web_cep.php?cep=" + $(".txtCEP").val() + "&formato=json",
+                url: 'http://republicavirtual.com.br/web_cep.php?cep=' + $('.txtCEP').val() + '&formato=json',
                 // Antes de enviar, mostrar o gif de carregamento
                 beforeSend: function() {
-                    $(".img-loading").toggle("slow");
+                    $('.img-loading').toggle('slow');
                 }
-            }).done(function(data) { // A função done recebe um callback com o argumento "data", que é um object com os valores de retorno da requisição
+            }).done(function(data) { // A função done recebe um callback com o argumento 'data', que é um object com os valores de retorno da requisição
                 console.log(data.resultado);
                 if (data.resultado == 1) { // Se a pesquisa teve um resultado
-                    $(".cidade").text(data.cidade);
-                    $(".uf").text(data.uf);
-                    $(".bairro").text(data.bairro);
-                    $(".img-loading").toggle("slow");
-                    $(".result").show();
+                    $('.cidade').text(data.cidade);
+                    $('.uf').text(data.uf);
+                    $('.bairro').text(data.bairro);
+                    $('.img-loading').toggle('slow');
+                    $('.result').show();
                 }
                 else { // Se a pesquisa não deu resultado
-                    $(".img-loading").toggle();
-                    alert("CEP inválido ou inexistente!");
-                    $(".result").hide();
+                    $('.img-loading').toggle();
+                    alert('CEP inválido ou inexistente!');
+                    $('.result').hide();
                 }
             });
         });
